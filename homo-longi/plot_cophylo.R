eb5f864c3d347bbde5810ecb45f47cecb1787d7b
Chi Zhang <zhangchicool@gmail.com>
2025-10-25 16:06:19 +0800

    better comments

--- homo-longi/plot_cophylo.R
+++ homo-longi/plot_cophylo.R
@@ -10,12 +10,12 @@ tree_ni21 <- read.nexus("ni2021.con.tre")
 # continuous and discrete, unlinked WN clock
 tree_c_wn2 <- read.nexus("tipdating_wn_2r/data_both.con.tre")
 
-# continuous and discrete, unlinked WN clock
+# discretized and discrete, unlinked WN clock
 tree_d_wn2 <- read.nexus("tipdating_wn_2r/data_disc.con.tre")
 
 # plot tanglegrams
 plot(cophylo(tree_c_wn2, tree_ni21), fsize = 0.6)
-plot(cophylo(tree_ni21, tree_d_wn2), fsize = 0.6)
+plot(cophylo(tree_ni21, tree_d_wn2), fsize = 0.6)  # not included
 
 # compute the Robinson-Foulds distance for reference
 library(phangorn)
