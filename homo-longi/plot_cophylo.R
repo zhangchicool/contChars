060eec62eaa87f012fce4a486a141e8fd5c811bf
Chi Zhang <zhangchicool@gmail.com>
2025-10-25 15:51:11 +0800

    updated fig. 4 for tanglegram of homo trees

---
+++ homo-longi/plot_cophylo.R
@@ -0,0 +1,23 @@
+library(ape)
+library(phytools)
+
+setwd("~/Documents/Research/2025-ContChars/homo-longi")
+
+# dated tree from Ni et al 2021
+tree_ni21 <- read.nexus("ni2021.con.tre")
+# plot(tree_ni21, main = "Ni et al. 2021")
+
+# continuous and discrete, unlinked WN clock
+tree_c_wn2 <- read.nexus("tipdating_wn_2r/data_both.con.tre")
+
+# continuous and discrete, unlinked WN clock
+tree_d_wn2 <- read.nexus("tipdating_wn_2r/data_disc.con.tre")
+
+# plot tanglegrams
+plot(cophylo(tree_c_wn2, tree_ni21), fsize = 0.6)
+plot(cophylo(tree_ni21, tree_d_wn2), fsize = 0.6)
+
+# compute the Robinson-Foulds distance for reference
+library(phangorn)
+RF.dist(tree_ni21, tree_c_wn2)
+RF.dist(tree_ni21, tree_d_wn2)
